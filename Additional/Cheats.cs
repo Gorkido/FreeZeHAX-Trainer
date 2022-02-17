849b1884b2947c9fa34ced07202e6dd50fd9bff3
Gorkido <Gorkido@GORKIDO>
2022-02-17 16:02:36 +0300

    Trainer icon's file size is reduced

--- Additional/Cheats.cs
+++ Additional/Cheats.cs
@@ -2,38 +2,6 @@
 {
     public class Cheats
     {
-        /*public string[] GTCheats =
-        {
-            "27C63", // Force FPS | 0 Y
-            "3DCAC4", // Ban Bypass | 1 Y
-            "88CB00", // FPS String | 2 Y
-            "53C9C7", // Mod Fly V1 | 3 Y
-            "\n \nFreeZeHAX Trainer V3.78 \nBan Bypass: ON \nFps:%d                                   ", //FPS String's Text | 4 Y
-            "4CF333", // Anti int check | 5 Y
-            "53D97A", // Anti Bounce V3 | 6 Y
-            "53C9F0", // Fast Fall V1 | 7 Y
-            "4E8389", // Fast Fall V2 | 8 Y
-            "4DD1D1", // Growz | 9 Y
-            "52CFC5", // Noclip | 10 Y
-            "3CE4DF", // Mod Zoom |11 Y
-            "4DE1F4", // Slide Mode | 12 Y
-            "4DE202", // Anti Slide Mode | 13 Y
-            "4E4689", // Anti Knockback 1 | 14 Y
-            "4E48BA", // Anti Knockback 2 | 15 Y
-            "4DA3DE", // Anti Lgrid | 16 Y
-            "A6951", // System Speed 1 | 17 Y
-            "A66F1", // System Speed 2 | 18 Y
-            "A69D6", // System Speed 3 | 19 Y
-            "52CFD5", // Anti Platform | 20 Y
-            "4CE326", // Anti Gravity Well | 21 Y
-            "3DFBC1", // Dev Mode | 22 Y
-            "4DC942", // Ghost Mode | 23 Y
-            "4E8A32", // Gravity Mode | 24 Y
-            "5388B2", // Fast Pickup | 25 Y
-            "53892D", // Fast Drop | 26 Y
-            "4D3E65" // Anti Bounce Bypass | 27 Y
-        };*/
-
         public string[] GTCheats =
         {
             "0f 84 ? ? ? ? e8 ? ? ? ? 48 8b d8 8b be", // Force FPS | 0 Y
