610bb2e831a029fef720bef82bc57ba330a6ee0e
Gorkido <Gorkido@GORKIDO>
2022-02-27 02:42:47 +0300

    2022

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FreeZeHAX Trainer")]
-[assembly: AssemblyCopyright("Copyright © 2021 FreeZeHAX")]
+[assembly: AssemblyCopyright("Copyright © 2022 FreeZeHAX")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
